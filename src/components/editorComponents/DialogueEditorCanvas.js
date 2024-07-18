ff57238c6953c008fb6bd0d6f8eaa33b7ea41693
Dominik Pavlíček <dominikpavlicek.business@gmail.com>
2024-07-18 22:38:43 +0200

    Added ReactFlow

--- src/components/editorComponents/DialogueEditorCanvas.js
+++ src/components/editorComponents/DialogueEditorCanvas.js
@@ -1,12 +1,47 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useCallback } from 'react';
+import ReactFlow, { addEdge, MiniMap, Controls, Background, applyNodeChanges, applyEdgeChanges } from 'reactflow';
 
-import "../../componentStyles/editorComponentStyles/DialogueEditorCanvas.css"
+import 'reactflow/dist/style.css';
+import "../../componentStyles/editorComponentStyles/DialogueEditorCanvas.css";
 
-function DialogueEditorCanvas() {
+const initialNodes = [
+  { id: '1', type: 'input', data: { label: 'Start Node' }, position: { x: 250, y: 0 } }
+];
 
-    return (
-        <div className="dialogue-editor-canvas background-transparent-primary"></div>
-    )
+const initialEdges = [];
+
+const DialogueEditorCanvas = () => {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    []
+  );
+
+  return (
+    <div className="dialogue-editor-canvas background-transparent-primary">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <MiniMap />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 };
 
 export default DialogueEditorCanvas;
