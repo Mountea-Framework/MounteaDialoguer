a351ddb8ec69361b28fcfabdd39cea825a2a17d6
Dominik Pavlíček <dominikpavlicek.business@gmail.com>
2024-07-19 18:39:04 +0200

    Updated Scrollbox

--- src/components/editorComponents/DialogueEditorToolbar.js
+++ src/components/editorComponents/DialogueEditorToolbar.js
@@ -1,12 +1,28 @@
 import React, { useState, useContext } from "react";
 
-import "../../componentStyles/editorComponentStyles/DialogueEditorToolbar.css"
+import Button from "../../components/objects/Button";
+
+import "../../componentStyles/editorComponentStyles/DialogueEditorToolbar.css";
 
 function DialogueEditorTooblar() {
-
-    return (
-        <div className="dialogue-editor-toolbar background-secondary"></div>
-    )
-};
+  return (
+    <div className="dialogue-editor-toolbar background-secondary">
+      <Button
+        containerClassName={"toolbar-button-container"}
+        className={"custom-button toolbar-button"}
+        classState={"tertiary"}
+      >
+        save
+      </Button>
+      <Button
+        containerClassName={"toolbar-button-container"}
+        className={"custom-button toolbar-button"}
+        classState={"tertiary"}
+      >
+        load
+      </Button>
+    </div>
+  );
+}
 
 export default DialogueEditorTooblar;
