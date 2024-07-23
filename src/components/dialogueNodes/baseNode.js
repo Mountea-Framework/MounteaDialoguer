4e492cd783487c47acb4d9e4e4bcd9cd819de7d8
Dominik Pavlíček <dominikpavlicek.business@gmail.com>
2024-07-23 14:51:41 +0200

    Node ID

--- src/components/dialogueNodes/baseNode.js
+++ src/components/dialogueNodes/baseNode.js
@@ -1,10 +1,18 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { Handle } from "reactflow";
+
 import Title from "../objects/Title";
 
 const BaseNode = ({ data }) => {
   const { customClassName, title, targetHandle, sourceHandle } = data;
+  const [nodeId, setNodeId] = useState(data.nodeId || uuidv4());
+
+  useEffect(() => {
+    if (!data.nodeId) {
+      data.nodeId = nodeId;
+    }
+  }, [data, nodeId]);
 
   return (
     <div className="custom-node-border">
