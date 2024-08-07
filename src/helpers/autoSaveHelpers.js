06e27f4f565496ef948486f59b2a2d3850bb0f78
Dominik Pavlíček <dominikpavlicek.business@gmail.com>
2024-08-07 18:14:16 +0200

    Indexed DB

--- src/helpers/autoSaveHelpers.js
+++ src/helpers/autoSaveHelpers.js
@@ -6,7 +6,16 @@ const mergeWithExistingData = async (newData) => {
 	const projectsStore = tx.objectStore("projects");
 	const guid = localStorage.getItem("project-guid");
 	const existingData = (await projectsStore.get(guid)) || {};
-	return { ...existingData, ...newData };
+	return {
+		...existingData,
+		...newData,
+		name: existingData.name || newData.name,
+		categories: newData.categories || existingData.categories,
+		participants: newData.participants || existingData.participants,
+		nodes: newData.nodes || existingData.nodes,
+		edges: newData.edges || existingData.edges,
+		files: newData.files || existingData.files,
+	};
 };
 
 export default mergeWithExistingData;
