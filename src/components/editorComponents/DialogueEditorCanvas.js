6777e5b29e7cd144bf7b7fb9ef64917ee588eb9c
Dominik Pavlíček <dominikpavlicek.business@gmail.com>
2024-08-26 22:21:21 +0200

    Node Connection

--- src/components/editorComponents/DialogueEditorCanvas.js
+++ src/components/editorComponents/DialogueEditorCanvas.js
@@ -1,17 +1,17 @@
 import React, {
-	useState,
-	useCallback,
-	useContext,
-	useRef,
-	useEffect,
-	useMemo,
+  useState,
+  useCallback,
+  useContext,
+  useRef,
+  useEffect,
+  useMemo,
 } from "react";
 import ReactFlow, {
-	Controls,
-	reconnectEdge,
-	addEdge,
-	SelectionMode,
-	useReactFlow,
+  Controls,
+  reconnectEdge,
+  addEdge,
+  SelectionMode,
+  useReactFlow,
 } from "reactflow";
 import { v4 as uuidv4 } from "uuid";
 
@@ -35,301 +35,329 @@ import "../../componentStyles/editorComponentStyles/DialogueEditorCanvas.css";
 import "../../base/BaseNodesStyle.css";
 
 const nodeTypes = {
-	startNode: StartNode,
-	leadNode: LeadNode,
-	answerNode: AnswerNode,
-	closeDialogueNode: CloseDialogueNode,
-	jumpToNode: JumpToNode,
+  startNode: StartNode,
+  leadNode: LeadNode,
+  answerNode: AnswerNode,
+  closeDialogueNode: CloseDialogueNode,
+  jumpToNode: JumpToNode,
 };
 
 const edgeTypes = {
-	customEdge: CustomEdge,
+  customEdge: CustomEdge,
 };
 
 const panOnDrag = [1, 2];
 
 const DialogueEditorCanvas = ({
-	nodes,
-	edges,
-	setNodes,
-	setEdges,
-	onNodesChange,
-	onEdgesChange,
+  nodes,
+  edges,
+  setNodes,
+  setEdges,
+  onNodesChange,
+  onEdgesChange,
 }) => {
-	const { name, categories, participants } = useContext(AppContext);
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [contextMenuPosition, setContextMenuPosition] = useState({
-		x: 0,
-		y: 0,
-	});
-	const [isDragging, setIsDragging] = useState(false); // Add dragging state
-	const { selectedNode, selectNode } = useSelection();
+  const { name, categories, participants } = useContext(AppContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [contextMenuPosition, setContextMenuPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [isDragging, setIsDragging] = useState(false);
+  const { selectedNode, selectNode } = useSelection();
 
-	const reactFlowWrapper = useRef(null);
-	const { project } = useReactFlow();
+  const reactFlowWrapper = useRef(null);
+  const { project } = useReactFlow();
 
-	const onNodesDelete = useCallback(
-		(nodesToDelete) => {
-			setNodes((prevNodes) => {
-				const updatedNodes = prevNodes.filter(
-					(node) => !nodesToDelete.some((n) => n.id === node.id)
-				);
+  const onNodesDelete = useCallback(
+    (nodesToDelete) => {
+      setNodes((prevNodes) => {
+        const updatedNodes = prevNodes.filter(
+          (node) => !nodesToDelete.some((n) => n.id === node.id)
+        );
 
-				// Check if the deleted node was selected
-				if (nodesToDelete.some((node) => node.id === selectedNode?.id)) {
-					selectNode(null);
-				}
+        if (nodesToDelete.some((node) => node.id === selectedNode?.id)) {
+          selectNode(null);
+        }
 
-				return updatedNodes;
-			});
-		},
-		[setNodes, selectedNode, selectNode]
-	);
+        return updatedNodes;
+      });
+    },
+    [setNodes, selectedNode, selectNode]
+  );
 
-	const handleNodesChange = useCallback(
-		(changes) => {
-			onNodesChange(changes);
+  const handleNodesChange = useCallback(
+    (changes) => {
+      onNodesChange(changes);
 
-			// After the changes are applied, check if the selected node still exists
-			setNodes((currentNodes) => {
-				if (
-					selectedNode &&
-					!currentNodes.some((node) => node.id === selectedNode.id)
-				) {
-					selectNode(null);
-				}
-				return currentNodes;
-			});
-		},
-		[onNodesChange, selectedNode, selectNode, setNodes]
-	);
+      // After the changes are applied, check if the selected node still exists
+      setNodes((currentNodes) => {
+        if (
+          selectedNode &&
+          !currentNodes.some((node) => node.id === selectedNode.id)
+        ) {
+          selectNode(null);
+        }
+        return currentNodes;
+      });
+    },
+    [onNodesChange, selectedNode, selectNode, setNodes]
+  );
 
-	const handleNodeClick = useCallback(
-		(event, node) => {
-			if (selectedNode?.id !== node.id) {
-				selectNode(node);
-				setNodes((nds) => {
-					const needsUpdate = nds.some(
-						(n) =>
-							(n.id === node.id && !n.data.selected) ||
-							(n.id !== node.id && n.data.selected)
-					);
+  const handleNodeClick = useCallback(
+    (event, node) => {
+      if (selectedNode?.id !== node.id) {
+        selectNode(node);
+        setNodes((nds) => {
+          const needsUpdate = nds.some(
+            (n) =>
+              (n.id === node.id && !n.data.selected) ||
+              (n.id !== node.id && n.data.selected)
+          );
 
-					if (needsUpdate) {
-						return nds.map((n) => ({
-							...n,
-							data: {
-								...n.data,
-								selected: n.id === node.id,
-								isDragging,
-							},
-						}));
-					}
-					return nds;
-				});
-			}
-		},
-		[selectedNode, selectNode, setNodes, isDragging]
-	);
+          if (needsUpdate) {
+            return nds.map((n) => ({
+              ...n,
+              data: {
+                ...n.data,
+                selected: n.id === node.id,
+                isDragging,
+              },
+            }));
+          }
+          return nds;
+        });
+      }
+    },
+    [selectedNode, selectNode, setNodes, isDragging]
+  );
 
-	const handleNodeDoubleClick = useCallback(
-		(event, node) => {
-			selectNode(node);
-			setNodes((nds) => {
-				const needsUpdate = nds.some(
-					(n) =>
-						(n.id === node.id && !n.data.selected) ||
-						(n.id !== node.id && n.data.selected)
-				);
+  const handleNodeDoubleClick = useCallback(
+    (event, node) => {
+      selectNode(node);
+      setNodes((nds) => {
+        const needsUpdate = nds.some(
+          (n) =>
+            (n.id === node.id && !n.data.selected) ||
+            (n.id !== node.id && n.data.selected)
+        );
 
-				if (needsUpdate) {
-					return nds.map((n) => ({
-						...n,
-						data: {
-							...n.data,
-							selected: n.id === node.id,
-							isDragging,
-						},
-					}));
-				}
-				return nds;
-			});
-		},
-		[selectNode, setNodes, isDragging]
-	);
+        if (needsUpdate) {
+          return nds.map((n) => ({
+            ...n,
+            data: {
+              ...n.data,
+              selected: n.id === node.id,
+              isDragging,
+            },
+          }));
+        }
+        return nds;
+      });
+    },
+    [selectNode, setNodes, isDragging]
+  );
 
-	const handlePaneContextMenu = (event) => {
-		event.preventDefault();
-		const { clientX, clientY } = event;
-		setContextMenuPosition({ x: clientX, y: clientY });
-		setIsModalOpen(true);
-		selectNode(null);
-	};
+  const handlePaneContextMenu = (event) => {
+    event.preventDefault();
+    const { clientX, clientY } = event;
+    setContextMenuPosition({ x: clientX, y: clientY });
+    setIsModalOpen(true);
+    selectNode(null);
+  };
 
-	const handlePaneClick = (event) => {
-		if (event.target === event.currentTarget) {
-			selectNode(null);
-			setNodes((nds) =>
-				nds.map((n) => ({
-					...n,
-					data: { ...n.data, selected: false, isDragging },
-				}))
-			);
-		}
-	};
+  const handlePaneClick = (event) => {
+    if (event.target === event.currentTarget) {
+      selectNode(null);
+      setNodes((nds) =>
+        nds.map((n) => ({
+          ...n,
+          data: { ...n.data, selected: false, isDragging },
+        }))
+      );
+    }
+  };
 
-	const handleDragNode = (event, node) => {
-		setIsDragging(true); // Set dragging state
-		if (node && selectedNode) {
-			if (node.id === selectedNode.id) {
-				return;
-			}
-		}
-		if (node !== selectedNode) {
-			selectNode(null);
-			setNodes((nds) =>
-				nds.map((n) => ({
-					...n,
-					data: { ...n.data, selected: false, isDragging: true },
-				}))
-			);
-		}
-	};
+  const handleDragNode = (event, node) => {
+    setIsDragging(true); // Set dragging state
+    if (node && selectedNode) {
+      if (node.id === selectedNode.id) {
+        return;
+      }
+    }
+    if (node !== selectedNode) {
+      selectNode(null);
+      setNodes((nds) =>
+        nds.map((n) => ({
+          ...n,
+          data: { ...n.data, selected: false, isDragging: true },
+        }))
+      );
+    }
+  };
 
-	const handleDragNodeEnd = (event, node) => {
-		setIsDragging(false);
-		if (selectedNode == null) {
-			setNodes((nds) =>
-				nds.map((n) => ({
-					...n,
-					data: { ...n.data, selected: false, isDragging: false },
-				}))
-			);
-		}
-	};
+  const handleDragNodeEnd = (event, node) => {
+    setIsDragging(false);
+    if (selectedNode == null) {
+      setNodes((nds) =>
+        nds.map((n) => ({
+          ...n,
+          data: { ...n.data, selected: false, isDragging: false },
+        }))
+      );
+    }
+  };
 
-	const handleSpawnNode = (type, label) => {
-		const projectedPosition = project(contextMenuPosition);
-		const newNode = {
-			id: uuidv4(),
-			type: type,
-			position: { x: projectedPosition.x, y: projectedPosition.y },
-			data: {
-				title: label, //`${type.charAt(0).toUpperCase() + type.slice(1)}`,
-				setEdges: setEdges,
-				selected: false,
-				isDragging: false,
-			},
-		};
-		setNodes((nds) => nds.concat(newNode));
-		setIsModalOpen(false);
-	};
+  const handleSpawnNode = (type, label) => {
+    const projectedPosition = project(contextMenuPosition);
+    const newNode = {
+      id: uuidv4(),
+      type: type,
+      position: { x: projectedPosition.x, y: projectedPosition.y },
+      data: {
+        title: label, //`${type.charAt(0).toUpperCase() + type.slice(1)}`,
+        setEdges: setEdges,
+        selected: false,
+        isDragging: false,
+      },
+    };
+    setNodes((nds) => nds.concat(newNode));
+    setIsModalOpen(false);
+  };
 
-	const onReconnect = useCallback(
-		(oldEdge, newConnection) =>
-			setEdges((els) => reconnectEdge(oldEdge, newConnection, els)),
-		[setEdges]
-	);
+  const onReconnect = useCallback(
+    (oldEdge, newConnection) =>
+      setEdges((els) => reconnectEdge(oldEdge, newConnection, els)),
+    [setEdges]
+  );
 
-	const onConnect = useCallback(
-		(connection) => {
-			const edge = { ...connection, type: "customEdge" };
-			setEdges((eds) => addEdge(edge, eds));
-		},
-		[setEdges]
-	);
+  const connectHandledRef = useRef(false);
+  const connectStartRef = useRef(null);
 
-	const isValidConnection = (connection) => {
-		return connection.source !== connection.target;
-	};
+  const onConnectStart = useCallback((_, { nodeId, handleType }) => {
+    connectStartRef.current = { nodeId, handleType };
+    connectHandledRef.current = false;
+  }, []);
 
-	useAutoSaveNodesAndEdges(nodes, edges);
-	useAutoSave(name, categories, participants);
+  const onConnect = useCallback(
+    (connection) => {
+      const edge = { ...connection, type: "customEdge" };
+      setEdges((eds) => addEdge(edge, eds));
+      connectHandledRef.current = true;
+    },
+    [setEdges]
+  );
 
-	useEffect(() => {
-		const handleContextMenu = (event) => {
-			event.preventDefault();
-		};
+  const handleConnectEnd = useCallback(
+    (event) => {
+      const targetIsPane = event.target.classList.contains("react-flow__pane");
+      const targetIsNode = event.target.classList.contains("react-flow__node");
 
-		const currentWrapper = reactFlowWrapper.current;
-		currentWrapper.addEventListener("contextmenu", handleContextMenu);
+      if (targetIsPane && !connectHandledRef.current) {
+        handlePaneContextMenu(event);
+      } else if (targetIsNode && !connectHandledRef.current) {
+        // If connection ended on a Node I want to check if it is already connected
+        // if not and try to connect
+      }
 
-		return () => {
-			currentWrapper.removeEventListener("contextmenu", handleContextMenu);
-		};
-	}, []);
+      connectStartRef.current = null;
+      connectHandledRef.current = false;
+    },
+    [handlePaneContextMenu]
+  );
 
-	// Memoize nodeTypes to prevent unnecessary re-renders
-	const memoizedNodeTypes = useMemo(() => {
-		return Object.fromEntries(
-			Object.entries(nodeTypes).map(([key, value]) => [key, value])
-		);
-	}, []);
+  const isValidConnection = (connection) => {
+    return connection.source !== connection.target;
+  };
 
-	const [nodeVersion, setNodeVersion] = useState({});
+  useAutoSaveNodesAndEdges(nodes, edges);
+  useAutoSave(name, categories, participants);
 
-	const forceNodeUpdate = useCallback((nodeId) => {
-		console.log("I will updated selected node!");
-		setNodeVersion((prev) => ({
-			...prev,
-			[nodeId]: (prev[nodeId] || 0) + 1,
-		}));
-	}, []);
+  useEffect(() => {
+    const handleContextMenu = (event) => {
+      event.preventDefault();
+    };
 
-	const memoizedNodes = useMemo(() => {
-		return nodes.map((node) => ({
-			...node,
-			data: {
-				...node.data,
-				isDragging,
-				version: nodeVersion[node.id] || 0,
-				forceNodeUpdate,
-			},
-		}));
-	}, [nodes, isDragging, nodeVersion, forceNodeUpdate]);
+    const currentWrapper = reactFlowWrapper.current;
+    currentWrapper.addEventListener("contextmenu", handleContextMenu);
 
-	return (
-		<div
-			className="dialogue-editor-canvas background-transparent-primary"
-			ref={reactFlowWrapper}
-			onContextMenu={handlePaneContextMenu}
-			onClick={handlePaneClick}
-		>
-			<ReactFlow
-				nodes={memoizedNodes}
-				edges={edges}
-				onNodesChange={handleNodesChange}
-				onEdgesChange={onEdgesChange}
-				onNodesDelete={onNodesDelete}
-				onConnect={onConnect}
-				isValidConnection={isValidConnection}
-				snapToGrid
-				snapGrid={[10, 10]}
-				onReconnect={onReconnect}
-				nodeTypes={memoizedNodeTypes}
-				edgeTypes={edgeTypes}
-				maxZoom={1.75}
-				minZoom={0.25}
-				fitView
-				selectionOnDrag
-				panOnDrag={panOnDrag}
-				selectionMode={SelectionMode.Partial}
-				onNodeDoubleClick={handleNodeDoubleClick}
-				onNodeClick={handleNodeClick}
-				onPaneClick={handlePaneClick}
-				onNodeDragStart={handleDragNode}
-				onNodeDragStop={handleDragNodeEnd}
-			>
-				<Controls />
-			</ReactFlow>
-			<SpawnNewNode
-				isOpen={isModalOpen}
-				onClose={() => setIsModalOpen(false)}
-				nodeTypes={memoizedNodeTypes}
-				onSpawn={handleSpawnNode}
-			/>
-		</div>
-	);
+    return () => {
+      currentWrapper.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
+  // Memoize nodeTypes to prevent unnecessary re-renders
+  const memoizedNodeTypes = useMemo(() => {
+    return Object.fromEntries(
+      Object.entries(nodeTypes).map(([key, value]) => [key, value])
+    );
+  }, []);
+
+  const [nodeVersion, setNodeVersion] = useState({});
+
+  const forceNodeUpdate = useCallback((nodeId) => {
+    console.log("I will updated selected node!");
+    setNodeVersion((prev) => ({
+      ...prev,
+      [nodeId]: (prev[nodeId] || 0) + 1,
+    }));
+  }, []);
+
+  const memoizedNodes = useMemo(() => {
+    return nodes.map((node) => ({
+      ...node,
+      data: {
+        ...node.data,
+        isDragging,
+        version: nodeVersion[node.id] || 0,
+        forceNodeUpdate,
+      },
+    }));
+  }, [nodes, isDragging, nodeVersion, forceNodeUpdate]);
+
+  return (
+    <div
+      className="dialogue-editor-canvas background-transparent-primary"
+      ref={reactFlowWrapper}
+      onContextMenu={handlePaneContextMenu}
+      onClick={handlePaneClick}
+    >
+      <ReactFlow
+        nodes={memoizedNodes}
+        edges={edges}
+        onNodesChange={handleNodesChange}
+        onEdgesChange={onEdgesChange}
+        onNodesDelete={onNodesDelete}
+        onConnect={onConnect}
+        onConnectEnd={handleConnectEnd}
+        onConnectStart={onConnectStart}
+        isValidConnection={isValidConnection}
+        snapToGrid
+        snapGrid={[10, 10]}
+        onReconnect={onReconnect}
+        nodeTypes={memoizedNodeTypes}
+        edgeTypes={edgeTypes}
+        maxZoom={1.75}
+        minZoom={0.25}
+        fitView
+        selectionOnDrag
+        panOnDrag={panOnDrag}
+        selectionMode={SelectionMode.Partial}
+        onNodeDoubleClick={handleNodeDoubleClick}
+        onNodeClick={handleNodeClick}
+        onPaneClick={handlePaneClick}
+        onNodeDragStart={handleDragNode}
+        onNodeDragStop={handleDragNodeEnd}
+      >
+        <Controls />
+      </ReactFlow>
+      <SpawnNewNode
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        nodeTypes={memoizedNodeTypes}
+        onSpawn={handleSpawnNode}
+      />
+    </div>
+  );
 };
 
 export default DialogueEditorCanvas;
